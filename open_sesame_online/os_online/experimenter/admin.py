67ffa4867728bb8740f1533dcbddef762419a3fe
BenBrucker <benbruecker@gmail.com>
2014-09-10 06:36:15 -0400

    Removed old files with longer name

--- open_sesame_online/os_online/experimenter/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from os_online.experimenter.models import Experiment, Result, Token
-
-admin.site.register(Experiment)
-admin.site.register(Result)
-admin.site.register(Token)
--- open_sesame_online/os_online/translator/unit_tests/test_all_examples.py
+++
@@ -1,9 +0,0 @@
-""" Runs pylint against all files in this directory"""
-import os
-import re
-import os_online.translator.translator as translator
-
-for f_name in os.listdir("examples"):
-    if f_name.endswith(".opensesame"):
-        translator.main('examples/' + f_name,\
-                        'examples.out/' + re.sub('.opensesame', '.js', f_name))
